d9c7159be71e7928876523da611de3fa391d95eb
TikhonovN86 <tikhonovns97@gmial.com>
2026-01-16 21:34:03 +0500

    файл java-basics.iml удален из индексации и добавлен в gitignore. Внесены исправленияпо замечаниям

--- src/org/itmo/lesson1/Main.java
+++ src/org/itmo/lesson1/Main.java
@@ -1,9 +1,4 @@
 package org.itmo.lesson1;
 
-import static org.itmo.lesson1.Solution.displayResults;
-
 public class Main {
-    public static void main(String[] args) {
-        displayResults();
-    }
 }
